fe8d935d3931164b82a57f50308dbbb7d24fe18d
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-12-18 09:28:44 +0000

    Create put method in profile detail class

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -22,6 +23,11 @@ class ProfileList(APIView):
 
 
 class ProfileDetail(APIView):
+    # Allows serialized code to be modified with a form
+    serializer_class = ProfileSerializer
+
+    # Simple get object method to obtain actual
+    # profile object or return an error page
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(
@@ -31,9 +37,30 @@ class ProfileDetail(APIView):
         except Profile.DoesNotExist:
             raise Http404
 
+    # Simple get method to obtain data and
+    # serialize said data into readable JSON code
     def get(self, request, pk):
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile)
         return Response(
             serializer.data
+        )
+
+    # Put method to obtain profile object via pk
+    # that allows us to modify data and save it
+    # or in the case of error, throws a 400 error
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(
+            profile,
+            data=request.data
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
         )
