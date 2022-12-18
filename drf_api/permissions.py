ba7116342ccd99426afeb7faff6b5955f52c5da5
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-12-18 09:38:57 +0000

    Create permissions.py in core file and create first permissions class

---
+++ drf_api/permissions.py
@@ -0,0 +1,13 @@
+"""
+File in which to create permissions for authenticated
+and/or non-authenticated users for serialized data
+"""
+
+from rest_framework import permissions
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user
