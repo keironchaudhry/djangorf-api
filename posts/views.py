55413b6344172a3bf09a80fd0fdf356c0c172866
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-12-19 19:07:01 +0000

    Create post method in post list view

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Post
@@ -6,6 +6,14 @@ from .serializers import PostSerializer
 
 
 class PostList(APIView):
+
+    # Creates a post form to be rendered on page
+    serializer_class = PostSerializer
+    # Permissions and authentication for user
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
     def get(self, request):
         # Like in profile app, retrieve all post objects,
         posts = Post.objects.all()
@@ -18,4 +26,26 @@ class PostList(APIView):
         # And return serialized data using Response
         return Response(
             serializer.data
+        )
+
+    def post(self, request):
+        # Deserialize the user data
+        serializer = PostSerializer(
+            data=request.data,
+            context={'request': request}
+        )
+        # Passes in save method on serializer
+        # and identifies the user making the request
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            # Returns the serialized data with
+            # 201 status is serializer is valid
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        # Else returns 400 bad request if invalid
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
         )
