cf7ff50f65716b8dda9d4fc8fe84bfe0b85b05ad
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-12-19 18:36:51 +0000

    Create get method for postlist class in views

--- posts/views.py
+++ posts/views.py
@@ -4,4 +4,18 @@ from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(APIView):
+    def get(self, request):
+        # Like in profile app, retrieve all post objects,
+        posts = Post.objects.all()
+        # Serialize them,
+        serializer = PostSerializer(
+            post,
+            many=True,
+            context={'request': request}
+        )
+        # And return serialized data using Response
+        return Response(
+            serializer.data
+        )
