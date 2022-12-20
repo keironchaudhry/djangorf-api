5114075f26ef762de9f579d807ee8ebd84791366
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-12-20 06:28:21 +0000

    Create setup method and test listing posts method

--- posts/tests.py
+++ posts/tests.py
@@ -4,4 +4,27 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(
+            username='test_user',
+            password='test_password'
+        )
 
+    def test_can_list_posts(self):
+        test_user = User.objects.get(
+            username='test_user'
+        )
+        Post.objects.create(
+            owner=test_user,
+            title='test title'
+        )
+        response = self.client.get(
+            '/posts/'
+        )
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+        print(response.data)
+        print(len(response.data))
