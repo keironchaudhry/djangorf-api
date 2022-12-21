8cb7316ba307351730fa84cf7186f989b0db851c
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-12-21 06:59:48 +0000

    Beautify python format and code

--- user_comments/models.py
+++ user_comments/models.py
@@ -7,10 +7,20 @@ class Comment(models.Model):
     """
     Comment model, related to User and Post
     """
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
     content = models.TextField()
 
     class Meta:
