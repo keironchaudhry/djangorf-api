c0d63662aeed46ed583c27d4ff8d17a12ab3d3de
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-12-21 07:42:35 +0000

    Create generics class list api view

--- user_comments/views.py
+++ user_comments/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import (
+    CommentSerializer,
+    CommentDetailSerializer
+)
 
-# Create your views here.
+# In this walkthrough, rather than repeating
+# the creation of a GET, PUT, POST, DELETE etc.,
+# we have imported 'generics' and using built in features
+# that enable the creation of these methods.
+
+
+class CommentList(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer()
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(
+            owner=self.request.user
+        )
