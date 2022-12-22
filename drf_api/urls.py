1a1ef70234a958dfe140cfdceeb88881a726badc
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-12-22 17:12:03 +0000

    Add all remaining dependencies for deployment

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -29,6 +29,9 @@ urlpatterns = [
     ),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
+        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
+    ),
+    path(
         '',
         include(
             'profiles.urls'
