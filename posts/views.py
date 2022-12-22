9b475783d1000b660358080cc84e2a676c14c9f7
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-12-22 15:06:47 +0000

    Fix import duplications and import django filters into post views

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
-from drf_api.permissions import IsOwnerOrReadOnly
-from .models import Post
-from .serializers import PostSerializer
-
-
-from django.db.models import Count
-from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
