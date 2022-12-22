7c6002c58f9593d526dbb8d915ea1e88bc07d656
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-12-22 15:55:13 +0000

    Modify code to render filter tab

--- profiles/views.py
+++ profiles/views.py
@@ -12,37 +12,24 @@ class ProfileList(generics.ListAPIView):
     No create view as profile creation is handled by django signals.
     """
     queryset = Profile.objects.annotate(
-        posts_count=Count(
-            'owner__post',
-            distinct=True
-        ),
-        followers_count=Count(
-            'owner__followed',
-            distinct=True
-        ),
-        following_count=Count(
-            'owner__following',
-            distinct=True
-        )
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
-
     serializer_class = ProfileSerializer
-
-    filters_backend = [
+    filter_backends = [
         filters.OrderingFilter,
-        DjangoFilterBackend
+        DjangoFilterBackend,
     ]
-
     filterset_fields = [
         'owner__following__followed__profile',
     ]
-
     ordering_fields = [
         'posts_count',
         'followers_count',
         'following_count',
         'owner__following__created_at',
-        'owner__followed__created_at'
+        'owner__followed__created_at',
     ]
 
 
@@ -52,17 +39,8 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.annotate(
-        posts_count=Count(
-            'owner__post',
-            distinct=True
-        ),
-        followers_count=Count(
-            'owner__followed',
-            distinct=True
-        ),
-        following_count=Count(
-            'owner__following',
-            distinct=True
-        )
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
