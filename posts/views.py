aa6a3093efda8c2319b14e8747f566c4338e9a57
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-12-22 15:49:09 +0000

    Create filter to be able to filter through fieldsets

--- posts/views.py
+++ posts/views.py
@@ -20,6 +20,12 @@ class PostList(generics.ListCreateAPIView):
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile'
     ]
     search_fields = [
         'owner__username',
