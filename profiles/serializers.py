d264a798765a7b5966f4836a65f400772bc99afa
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-12-22 07:09:50 +0000

    Create follower id method in serializers

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -7,6 +8,7 @@ class ProfileSerializer(serializers.ModelSerializer):
         source='owner.username'
     )
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
 
     # Allows to request owner identification of
     # profile object, and is referred to throughout
@@ -14,6 +16,17 @@ class ProfileSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_following_id(self, ob):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user,
+                followed=obj.owner
+            ).first()
+            # print(following)
+            return following.id if following else None
+        return None
 
     class Meta:
         model = Profile
@@ -26,4 +39,5 @@ class ProfileSerializer(serializers.ModelSerializer):
             'content',
             'image',
             'is_owner',
+            'following_id',
         ]
