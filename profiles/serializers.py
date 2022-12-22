9246d7e01421a5bc9bd5f162713acaa939246faf
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-12-22 07:39:33 +0000

    Modify queryset in profile views and annotate new fields

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -9,6 +9,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     )
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     # Allows to request owner identification of
     # profile object, and is referred to throughout
@@ -40,4 +43,7 @@ class ProfileSerializer(serializers.ModelSerializer):
             'image',
             'is_owner',
             'following_id',
+            'posts_count',
+            'followers_count',
+            'following_count'
         ]
