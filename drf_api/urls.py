e78b7dd30f049ee4f1ff81c851dd5a5af73a2a77
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-12-26 16:31:02 +0000

    Import code institute code to solve bug

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -18,66 +18,18 @@ from django.urls import path, include
 from .views import root_route, logout_route
 
 urlpatterns = [
+    path('', root_route),
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    # our logout route has to be above the default one to be matched first
+    path('dj-rest-auth/logout/', logout_route),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
-        '',
-        root_route
+        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
-    path(
-        'admin/',
-        admin.site.urls
-    ),
-    path(
-        'api-auth/',
-        include(
-            'rest_framework.urls'
-        )
-    ),
-    # logout route must be placed above
-    # default dj-rest-auth to be matched first
-    path(
-        'dj-rest-auth/logout/',
-        logout_route
-    ),
-    path(
-        'dj-rest-auth/',
-        include(
-            'dj_rest_auth.urls'
-        )
-    ),
-    path(
-        'dj-rest-auth/registration/',
-        include(
-            'dj_rest_auth.registration.urls'
-        )
-    ),
-    path(
-        '',
-        include(
-            'profiles.urls'
-        )
-    ),
-    path(
-        '',
-        include(
-            'posts.urls'
-        )
-    ),
-    path(
-        '',
-        include(
-            'user_comments.urls'
-        )
-    ),
-    path(
-        '',
-        include(
-            'likes.urls'
-        )
-    ),
-    path(
-        '',
-        include(
-            'followers.urls'
-        )
-    ),
+    path('', include('profiles.urls')),
+    path('', include('posts.urls')),
+    path('', include('comments.urls')),
+    path('', include('likes.urls')),
+    path('', include('followers.urls')),
 ]
--- drf_api/views.py
+++ drf_api/views.py
@@ -1,17 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .settings import (
-    JWT_AUTH_COOKIE,
-    JWT_AUTH_REFRESH_COOKIE,
-    JWT_AUTH_SAMESITE,
-    JWT_AUTH_SECURE
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
+    JWT_AUTH_SECURE,
 )
 
 
 @api_view()
 def root_route(request):
     return Response({
-        "message": "Welcome to my Django REST Framework API!"
+        "message": "Welcome to my drf API!"
     })
 
 
