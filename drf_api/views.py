cffc63fb2ccf0a8f5325e12b9fc5a0533b2e59f6
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-12-26 15:45:46 +0000

    Fix error in views file

--- drf_api/views.py
+++ drf_api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from .settings import (
     JWT_AUTH_COOKIE,
     JWT_AUTH_REFRESH_COOKIE,
-    JW_AUTH_SAMESITE,
+    JWT_AUTH_SAMESITE,
     JWT_AUTH_SECURE
 )
 
