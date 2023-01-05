7813dcf754f9487c410820e5eb348c9145c8baba
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-01-05 13:49:52 +0000

    Check code in serializers file in posts app

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,35 +1,27 @@
 from rest_framework import serializers
-from .models import Post
+from posts.models import Post
 from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(
-        source='owner.username'
-    )
+    owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(
-        source='owner.profile.id'
-    )
-    profile_image = serializers.ReadOnlyField(
-        source='owner.profile.image.url'
-    )
-    liked_id = serializers.SerializerMethodField()
-    comments_count = serializers.ReadOnlyField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
     likes_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
-        if value.size > 1024 * 1024 * 2:
-            raise serializers.ValidationError(
-                'Image size larger than 2MB!'
-            )
-        if value.size.width > 4096:
-            raise serializers.ValidationError(
-                'Image width larger than 4096px!'
-            )
-        if value.size.height > 4096:
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
             raise serializers.ValidationError(
                 'Image height larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
             )
         return value
 
@@ -37,31 +29,20 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
-    def get_liked_id(self, obj):
+    def get_like_id(self, obj):
         user = self.context['request'].user
         if user.is_authenticated:
             like = Like.objects.filter(
-                owner=user,
-                post=obj
+                owner=user, post=obj
             ).first()
-            return liked.id if like else None
+            return like.id if like else None
         return None
 
     class Meta:
         model = Post
         fields = [
-            'id',
-            'owner',
-            'is_owner',
-            'profile_id',
-            'profile_image',
-            'created_at',
-            'updated_at',
-            'title',
-            'content',
-            'image',
-            'image_filter',
-            'liked_id',
-            'comments_count',
-            'likes_count'
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            'title', 'content', 'image', 'image_filter',
+            'like_id', 'likes_count', 'comments_count',
         ]
